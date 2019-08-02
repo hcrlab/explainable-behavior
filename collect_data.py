11340eb0002d7b60074ddabc85397c63b83be577
Timothy Akintilo <timtobi1@gmail.com>
2019-08-01 18:54:50 -0700

    do it all in one loop, allow for custom folder names

--- collect_data.py
+++ collect_data.py
@@ -11,22 +11,26 @@ from cozmo.util import degrees
 from datetime import datetime
 
 
-def write_data(robot: cozmo.robot.Robot, append=False, verbose=False):
+def write_data(robot: cozmo.robot.Robot, frequency = 10.0, append=False, verbose=False):
     def add_speed(wheel_speeds: tuple, addition: tuple):
         return (wheel_speeds[0] + addition[0], wheel_speeds[1] + addition[1])
         
     pygame.init()
     screen = pygame.display.set_mode((100,100))
     
-    # all speeds in millimiters per second
+    # wheel speeds in millimiters per second
     # (left wheel speed, right wheel speed)
     go_forward = (100, 100)
     go_backward = (-100, -100)
     turn_left = (-50, 50)
     turn_right = (50, -50)
+    # head speeds in radians per second
+    tilt_up = 0.3
+    tilt_down = -0.3
 
     header = ["left wheel speed", "right wheel speed", "gyro x", "gyro y", "gyro z", "pose x", "pose y", "pose z",
-        "q0", "q1", "q2", "q3", "angle_z", "origin_id", "is_accurate", "up key", "down key", "left key", "right key", "timestamp"]
+        "q0", "q1", "q2", "q3", "angle_z", "origin_id", "is_accurate", "wheels: up key", "wheels: down key",
+        "wheels: left key", "wheels: right key", "head: up key", "head: down key" "timestamp"]
 
     # where jpg filenames should begin. start at 1 so filenames match rows in csv (accounting for header row)
     start_from = 1 
@@ -41,31 +45,50 @@ def write_data(robot: cozmo.robot.Robot, append=False, verbose=False):
         writer = csv.writer(file)
         if not append:
             writer.writerow(header)
-        while True: # should this be "while robot.EvtNewCameraImage:"?
-            try:
+        try:
+            period = 1/frequency # default: 0.1 sec
+            wheels = {"up": 0, "down": 0, "left": 0, "right": 0}
+            head = {"up": 0, "down": 0}
+            while True: # should this be "while robot.EvtNewCameraImage:"?
+                # start timer
+                start_time = time.time()
                 # drive cozmo
                 event = pygame.event.poll()
                 if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
                     keys = pygame.key.get_pressed()
-                    arrows = {"up": keys[pygame.K_UP], "down": keys[pygame.K_DOWN], "left": keys[pygame.K_LEFT], "right": keys[pygame.K_RIGHT]}
+                    wheels["up"] = keys[pygame.K_UP]
+                    wheels["down"] = keys[pygame.K_DOWN]
+                    wheels["left"] = keys[pygame.K_LEFT]
+                    wheels["right"] = keys[pygame.K_RIGHT]
+                    head["up"] = keys[pygame.K_w]
+                    head["down"] = keys[pygame.K_r] # colemak
+                    # head["down"] = keys[pygame.K_s] # QWERTY
                     # calculate wheel speeds
                     wheel_speeds = (0.0, 0.0)
                     # up arrow key
-                    if arrows["up"]:
+                    if wheels["up"]:
                         wheel_speeds = add_speed(wheel_speeds, go_forward)
                     # down arrow key
-                    if arrows["down"]:
+                    if wheels["down"]:
                         wheel_speeds = add_speed(wheel_speeds, go_backward)
                     # left arrow key
-                    if arrows["left"]:
+                    if wheels["left"]:
                         wheel_speeds = add_speed(wheel_speeds, turn_left)
                     # right arrow key
-                    if arrows["right"]:
+                    if wheels["right"]:
                         wheel_speeds = add_speed(wheel_speeds, turn_right)
+
+                    # calculate head speed
+                    head_speed = 0
+                    if head["up"]:
+                        head_speed += tilt_up
+                    if head["down"]:
+                        head_speed += tilt_down
                     
                     # send wheel speeds to cozmo
                     robot.drive_wheel_motors(wheel_speeds[0], wheel_speeds[1])
-                    print("sending speeds {} and {}".format(wheel_speeds[0], wheel_speeds[1]))
+                    # send head speed
+                    robot.move_head(head_speed)
 
                 # save data
                 img = robot.world.latest_image
@@ -83,27 +106,29 @@ def write_data(robot: cozmo.robot.Robot, append=False, verbose=False):
                     row = [robot.left_wheel_speed.speed_mmps, robot.right_wheel_speed.speed_mmps, gyro.x, gyro.y, gyro.z, datetime.now().time()]
                     row = [robot.pose.position, robot.pose.rotation]
                     row = [lws, rws, gyro.x, gyro.y, gyro.z, pose.position.x, pose.position.y, pose.position.z, pose.rotation.q0, pose.rotation.q1, pose.rotation.q2,
-                        pose.rotation.q3, pose.rotation.angle_z.radians, pose.origin_id, pose.is_accurate, arrows["up"], arrows["down"], arrows["left"],
-                        arrows["right"], present_time]
+                        pose.rotation.q3, pose.rotation.angle_z.radians, pose.origin_id, pose.is_accurate, wheels["up"], wheels["down"], wheels["left"],
+                        wheels["right"], head["up"], head["down"], present_time]
                     if verbose:
                         print("Saving data to row {}".format(end))
                     writer.writerow(row)
                     # update count
                     end += 1
 
-                # so we get ~10 images per second
-                # could try every 0.25 seconds instead? maybe 0.5?
-                time.sleep(0.1) 
+                # end timer
+                end_time = time.time()
+                # calculate how long to sleep to mantain desired frequency (default 10 Hz)
+                sample_time = start_time - end_time
+                if sample_time < period:
+                    time.sleep(period - sample_time)
+        except (KeyboardInterrupt, SystemExit):
+            print("Stop command received")
+            print("Saved {} new data points".format(end - begin + 1))
+        finally:
+            robot.stop_all_motors()
+            pygame.quit()
 
-        print("Stop command received")
-        print("Saved {} new data points".format(end - begin + 1))
 
-        robot.stop_all_motors()
-        pygame.quit()
-
-
-def collect_data(robot: cozmo.robot.Robot):
-    global finished
+def collect_data(robot: cozmo.robot.Robot, frequency = 10.0):
     """Collect LfD data.
 
     Arguments:
@@ -135,9 +160,23 @@ def collect_data(robot: cozmo.robot.Robot):
         os.chdir("{}/data/{}".format(absolute_path, folder_name))
         if verbose:
             print("Saving data in {} mode".format(mode))
-        write_data(robot, append=True if append_to else False, verbose=verbose)
+        write_data(robot, frequency, append=True if append_to else False, verbose=verbose)
+
+        os.chdir("..")
+        custom_name = input("What would you like to call this data folder? (leave empty to use {}){}".format(
+            "original name" if append_to else "timestamp", os.linesep))
+        while True:
+            if len(custom_name) > 0:
+                try:
+                    os.rename(folder_name, custom_name)
+                    break
+                except OSError:
+                    custom_name = input("That folder already exists. Try another name (leave empty to use {}):{}".format(
+                        "original name" if append_to else "timestamp", os.linesep))
+            else:
+                break
         
-        print("Data saved to {}".format(os.getcwd()))
+        print("Data saved to {}".format(custom_name if len(custom_name) > 0 else folder_name))
 
     except NotADirectoryError:
         print("Folder does not exist. Was it mistyped?")
@@ -149,7 +188,7 @@ def collect_data(robot: cozmo.robot.Robot):
 def main(robot: cozmo.robot.Robot):
     robot.set_head_angle(degrees(0)).wait_for_completed()
     robot.set_lift_height(0).wait_for_completed()
-    collect_data(robot)
+    collect_data(robot, 10)
 
 
 if __name__ == "__main__":
